f1ff54faa018fec1d10e0b89fe05b81267e8ac75
Nadija Smerechuk <nadija1smerechuk@gmail.com>
2024-09-27 08:52:34 +0300

    add page

---
+++ src/pages/ContactsPage/ContactsPage.jsx
@@ -0,0 +1,33 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import DocumentTitle from "../../components/DocumentTitle";
+import ContactList from "../../components/ContactList/ContactList";
+import { fetchContacts } from "../../redux/contacts/operation";
+import { selectContactsError, selectContactsLoading } from "../../redux/contacts/selectors";
+import SearchBox from "../../components/SearchBox/SearchBox";
+import Loader from "../../components/Loader/Loader";
+
+import css from "./ContactsPage.module.css";
+
+const ContactsPage = () => {
+  const dispatch = useDispatch();
+  const isLoading = useSelector(selectContactsLoading);
+  const error = useSelector(selectContactsError);
+
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
+
+  return (
+    <main >
+      <DocumentTitle>Phonebook</DocumentTitle>
+      <h1 >Phonebook</h1>
+      <SearchBox />
+      {isLoading && !error && <Loader />}
+      {error && <b>{error}</b>}
+      <ContactList />
+    </main>
+  );
+};
+
+export default ContactsPage;
