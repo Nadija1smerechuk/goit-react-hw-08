535d25d29781f4941753d42fcf989de459e8fd17
Nadija Smerechuk <nadija1smerechuk@gmail.com>
2024-09-27 21:33:08 +0300

    addgitign

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,8 +2,8 @@ import { useEffect, lazy } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Route, Routes } from "react-router-dom";
 import Layout from "../components/Layout/Layout";
-import PrivateRoute from "./RestrictedRoute";
-import RestrictedRoute from "./PrivateRoute";
+import PrivateRoute from "./PrivateRoute";
+import RestrictedRoute from "./RestrictedRoute";
 import { refreshUser } from "../redux/auth/operation";
 import { selectIsRefreshing } from "../redux/auth/selectors";
 
